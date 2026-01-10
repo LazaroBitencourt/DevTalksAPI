c841f47dc0b75aa75b2b95a67669910e01d9ab5d
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2026-01-09 23:00:11 -0300

    modifica entidades autor, category, user

--- src/main/java/com/github/LazaroBitencourt/DevTalksAPI/author/domain/Author.java
+++ src/main/java/com/github/LazaroBitencourt/DevTalksAPI/author/domain/Author.java
@@ -1,10 +1,13 @@
 package com.github.LazaroBitencourt.DevTalksAPI.author.domain;
 
-import jakarta.validation.constraints.Email;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
-@Email
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "BINARY(16)", name = "author_id", unique = true, nullable = false)
     UUID idAuthor;
 }
--- src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/domain/Category.java
+++ src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/domain/Category.java
@@ -1,10 +1,13 @@
 package com.github.LazaroBitencourt.DevTalksAPI.category.domain;
 
-import jakarta.validation.constraints.Email;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
-@Email
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "BINARY(16)", name = "category_id", unique = true, nullable = false)
     UUID idCategory;
 }
