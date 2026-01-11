28c0b8944830c41a2135b59b10d3eb82634c2164
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2026-01-11 18:36:51 -0300

    cria e implementa category-response

---
+++ src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/application/api/CategoryRespose.java
@@ -0,0 +1,17 @@
+package com.github.LazaroBitencourt.DevTalksAPI.category.application.api;
+
+import com.github.LazaroBitencourt.DevTalksAPI.category.domain.Status;
+import com.github.LazaroBitencourt.DevTalksAPI.post.domain.Post;
+
+import java.util.List;
+import java.util.UUID;
+
+public record CategoryRespose(
+        UUID idCategory,
+        String name,
+        String description,
+        byte[] image,
+        Status status,
+        List<Post> posts
+) {
+}
