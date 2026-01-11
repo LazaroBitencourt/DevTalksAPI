87121a73d1bb9230d46d3b29413a4428ab6146df
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2026-01-11 20:37:22 -0300

    implementa metodo em category-application-service

--- src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/application/service/CategoryService.java
+++ src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/application/service/CategoryService.java
@@ -3,11 +3,15 @@ package com.github.LazaroBitencourt.DevTalksAPI.category.application.service;
 import com.github.LazaroBitencourt.DevTalksAPI.category.application.api.CategoryIdResponse;
 import com.github.LazaroBitencourt.DevTalksAPI.category.application.api.CategoryRequest;
 import com.github.LazaroBitencourt.DevTalksAPI.category.application.api.CategoryRespose;
+import com.github.LazaroBitencourt.DevTalksAPI.category.application.api.ListCategoryResponse;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CategoryService {
     CategoryIdResponse createNewCategory(CategoryRequest categoryRequest);
 
     CategoryRespose findCategoryById(UUID idCategory);
+
+    List<ListCategoryResponse> listCategories();
 }
