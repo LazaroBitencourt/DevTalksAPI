21289cf97d53f83c80501c2d7f315954d86ca605
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2026-01-11 18:56:45 -0300

    cria metodo buscar categoria por id em category-application service na camada service

--- src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/application/service/CategoryService.java
+++ src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/application/service/CategoryService.java
@@ -2,7 +2,12 @@ package com.github.LazaroBitencourt.DevTalksAPI.category.application.service;
 
 import com.github.LazaroBitencourt.DevTalksAPI.category.application.api.CategoryIdResponse;
 import com.github.LazaroBitencourt.DevTalksAPI.category.application.api.CategoryRequest;
+import com.github.LazaroBitencourt.DevTalksAPI.category.application.api.CategoryRespose;
+
+import java.util.UUID;
 
 public interface CategoryService {
     CategoryIdResponse createNewCategory(CategoryRequest categoryRequest);
+
+    CategoryRespose findCategoryById(UUID idCategory);
 }
