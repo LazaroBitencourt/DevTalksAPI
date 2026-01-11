2d458147116bbc8e18c9bc015d5c151def46a4dd
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2026-01-11 20:36:12 -0300

    cria e implementa contrutor, e metodo de conversão na classe dto list-category-response na camada api

--- src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/application/api/ListCategoryResponse.java
+++ src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/application/api/ListCategoryResponse.java
@@ -1,8 +1,11 @@
 package com.github.LazaroBitencourt.DevTalksAPI.category.application.api;
 
+import com.github.LazaroBitencourt.DevTalksAPI.category.domain.Category;
 import com.github.LazaroBitencourt.DevTalksAPI.category.domain.Status;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public record ListCategoryResponse(
         UUID idCategory,
@@ -11,4 +14,21 @@ public record ListCategoryResponse(
         byte[] image,
         Status status
 ) {
+    public ListCategoryResponse(Category category) {
+        this(
+                category.getIdCategory(),
+                category.getName(),
+                category.getDescription(),
+                category.getImage(),
+                category.getStatus()
+        );
+    }
+
+    public static List<ListCategoryResponse> convertToListOfCategoryResponse(List<Category> categories) {
+        return categories.stream().map(
+                        category -> new ListCategoryResponse(category))
+                .collect(Collectors
+                        .toList());
+    }
+
 }
