b56128f814e66db0dbe696b5821fea67a0f380b4
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2026-01-11 00:25:55 -0300

    implementa category-infra-repository, criar Category-spring-data-jpa-repository na camada de infra

--- src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/infra/CategoryInfraRepository.java
+++ src/main/java/com/github/LazaroBitencourt/DevTalksAPI/category/infra/CategoryInfraRepository.java
@@ -1,15 +1,31 @@
 package com.github.LazaroBitencourt.DevTalksAPI.category.infra;
 
 import com.github.LazaroBitencourt.DevTalksAPI.category.application.repository.CategoryRepository;
+import com.github.LazaroBitencourt.DevTalksAPI.category.application.service.CategoryService;
 import com.github.LazaroBitencourt.DevTalksAPI.category.domain.Category;
+import com.github.LazaroBitencourt.DevTalksAPI.handler.APIException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class CategoryInfraRepository implements CategoryRepository {
+    private final CategorySpringDataJpaRepository jpaRepository;
+
     @Override
     public Category save(Category category) {
-        return null;
+        log.info("[start] CategoryApplicationService - save");
+        try{
+            jpaRepository.save(category);
+        }catch (DataIntegrityViolationException e){
+            throw APIException.build(HttpStatus.BAD_REQUEST,"THERE ARE DUPLICATE DATA", e);
+        }
+
+        log.info("[finish] CategoryInfraRepository - save");
+        return category;
     }
 }
