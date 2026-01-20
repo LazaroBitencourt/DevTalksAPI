21a1d0e13bc9aff6eefae8423a04ad882809fc50
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2026-01-19 22:29:36 -0300

    implementa metodo na classe uploadImpl na pasta util com logica de upload de de imagens

--- src/main/java/com/github/LazaroBitencourt/DevTalksAPI/util/UploadImpl.java
+++ src/main/java/com/github/LazaroBitencourt/DevTalksAPI/util/UploadImpl.java
@@ -1,6 +1,8 @@
 package com.github.LazaroBitencourt.DevTalksAPI.util;
 
+import com.github.LazaroBitencourt.DevTalksAPI.handler.APIException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,32 +12,49 @@ import java.io.FileOutputStream;
 import java.util.UUID;
 
 @Component
-public class UploadImageImpl implements UploadImage {
+public class UploadImpl implements Upload {
+
     @Value("${path.upload.diretory}")
-    private String ServerUploadDirectory;
+    public String ServerUploadDirectoryRoot;
+
+    public enum DitetoryType {
+        CATEGORY("category"),
+        USER("user"),
+        POST("post");
+        private final String value;
+
+        DitetoryType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
 
     @Override
-    public boolean uploadImage(MultipartFile file, UUID id) throws Exception {
+    public String uploadImage(MultipartFile file, UUID id, UploadImpl.DitetoryType ditetoryType) {
 
         if (!file.isEmpty() && id != null) {
-            String fileName = id.toString() + "_" + file.getOriginalFilename();
+            String fileName = id + "_" + file.getOriginalFilename();
             try {
-                File diretory = new File(ServerUploadDirectory);
+                File diretory = new File(ServerUploadDirectoryRoot, ditetoryType.getValue());
                 if (!diretory.exists()) {
                     diretory.mkdirs();
-                    File serveFile = new File(diretory.getAbsolutePath() + File.separator + fileName);
-                    BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serveFile));
-                    stream.write(file.getBytes());
-                    stream.close();
-                    return true;
                 }
+                File serveFile = new File(diretory.getAbsolutePath() + File.separator + fileName);
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serveFile));
+                stream.write(file.getBytes());
+                stream.close();
+                return "/upload/" + ditetoryType.getValue() + "/" + fileName;
+
             } catch (Exception ex) {
-                throw new Exception("AN ERROR OCCURRED WHILE SAVING THE IMAGE TO THE SERVER");
+                throw APIException.build(HttpStatus.INTERNAL_SERVER_ERROR, "AN ERROR OCCURRED WHILE SAVING THE IMAGE TO THE SERVER", ex);
             }
-        }else if{
-            throw new Exception("INVALID UPLOAD REQUEST FILE IS EMPTY OR ID NOT PROVIDED");
+        } else {
+            throw APIException.build(HttpStatus.INTERNAL_SERVER_ERROR, "INVALID UPLOAD REQUEST FILE IS EMPTY OR ID NOT PROVIDED");
         }
-       return false;
     }
 }
+
 
