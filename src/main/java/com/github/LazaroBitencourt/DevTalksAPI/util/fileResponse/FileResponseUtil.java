1264f6350ade0b5eed1c2d8cd8338677cdf92e89
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2026-01-24 18:23:26 -0300

    cria interface e implementa classe de file-response

--- src/main/java/com/github/LazaroBitencourt/DevTalksAPI/util/fileResponse/FileResponseUtil.java
+++ src/main/java/com/github/LazaroBitencourt/DevTalksAPI/util/fileResponse/FileResponseUtil.java
@@ -1,4 +1,9 @@
-package com.github.LazaroBitencourt.DevTalksAPI.util;
+package com.github.LazaroBitencourt.DevTalksAPI.util.fileResponse;
 
-public class FileResponseUtil {
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.Resource;
+
+public interface FileResponseUtil {
+    public String getContentType(HttpServletRequest request, Resource resource);
+    public String getHeaderValue(Resource resource);
 }
