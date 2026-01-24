97543c9f3bdeb6ab1d917885ab325b139987f812
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2026-01-24 20:49:01 -0300

    implenta contrato de controller na camada api

--- src/main/java/com/github/LazaroBitencourt/DevTalksAPI/user/application/api/UserAPI.java
+++ src/main/java/com/github/LazaroBitencourt/DevTalksAPI/user/application/api/UserAPI.java
@@ -1,7 +1,10 @@
 package com.github.LazaroBitencourt.DevTalksAPI.user.application.api;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.UUID;
@@ -36,4 +39,8 @@ public interface UserAPI {
     @PostMapping("/{idUser}/upload")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void postUploadImageUser(@PathVariable("idUser") UUID idUser,@RequestParam("file") MultipartFile file);
+
+    @GetMapping("/download/{fileName:.+}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public ResponseEntity<Resource> getDownloadImageUser(@PathVariable("fileName") String fileName , HttpServletRequest request);
 }
